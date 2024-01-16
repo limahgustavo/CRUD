10fa49e6bad38d29cbc9d1d887870d5ffdef2307
limahgustavo <gustavo.limafgz@gmail.com>
2024-01-16 17:53:34 -0400

    Crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UserListComponent
+  },
+  {
+    path: 'user',
+    component: UserListComponent
+  },
+  {
+    path: 'user/create',
+    component: UserFormComponent
+  },
+  {
+    path: 'user/edit/:id',
+    component: UserFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
